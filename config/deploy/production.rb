7a66d973ef4bd606168c0e94595f4bfbba677935
valnech <valnech.m@gmail.com>
2014-12-29 18:15:57 +0200

    deploy host changed

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,4 +1,4 @@
 set :stage, :production
 
-role :app, %w{lb4.allbestbets.com}
-server 'lb4.allbestbets.com', user: 'samples.allbestbets.ru', roles: %w{app}, port: 65321
+role :app, %w{lb1.3bagels.com}
+server 'lb1.3bagels.com', user: 'samples.allbestbets.ru', roles: %w{app}, port: 65321
